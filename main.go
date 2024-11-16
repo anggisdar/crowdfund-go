1115518e43d79447a1d88a56862abefad9df2241
anggis <anggisdar@gmail.com>
2024-11-16 15:11:20 +0700

    connection to db

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:05072001@tcp(localhost:3306)/dbgo"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("Connection to database conected")
+}
