bba21575d27f690db1a397b5982e95989782873f
anggis <anggisdar@gmail.com>
2024-11-17 17:06:02 +0700

    gorm get to json

--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"webfunding/user"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
+	// 	dsn := "root:05072001@tcp(localhost:3306)/dbgo"
+	// 	dbgo, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// 	if err != nil {
+	// 		log.Fatal(err.Error())
+	// 	}
+
+	// 	fmt.Println("Connection to database conected")
+
+	// 	var users []user.User
+	// 	dbgo.Find(&users)
+
+	// 	for _, user := range users {
+	// 		fmt.Println(user.Name)
+	// 		fmt.Println(user.Email)
+	// 	}
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context) {
 	dsn := "root:05072001@tcp(localhost:3306)/dbgo"
 	dbgo, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -17,17 +40,8 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Connection to database conected")
-
 	var users []user.User
-
 	dbgo.Find(&users)
 
-	length := len(users)
-	fmt.Println(length)
-
-	for _, user := range users {
-		fmt.Println(user.Name)
-		fmt.Println(user.Email)
-	}
+	c.JSON(http.StatusOK, users)
 }
