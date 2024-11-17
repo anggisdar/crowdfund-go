328a10356f6bfcbaa874e80f3311bccb46514e5c
anggis <anggisdar@gmail.com>
2024-11-17 18:51:33 +0700

    update terbaru

--- main.go
+++ main.go
@@ -2,37 +2,13 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"webfunding/user"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// 	dsn := "root:05072001@tcp(localhost:3306)/dbgo"
-	// 	dbgo, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	// 	if err != nil {
-	// 		log.Fatal(err.Error())
-	// 	}
-
-	// 	fmt.Println("Connection to database conected")
-
-	// 	var users []user.User
-	// 	dbgo.Find(&users)
-
-	// 	for _, user := range users {
-	// 		fmt.Println(user.Name)
-	// 		fmt.Println(user.Email)
-	// 	}
-	router := gin.Default()
-	router.GET("/handler", handler)
-	router.Run()
-}
-
-func handler(c *gin.Context) {
 	dsn := "root:05072001@tcp(localhost:3306)/dbgo"
 	dbgo, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -40,8 +16,10 @@ func handler(c *gin.Context) {
 		log.Fatal(err.Error())
 	}
 
-	var users []user.User
-	dbgo.Find(&users)
+	userRepository := user.NewRepository(dbgo)
+	user := user.User{
+		Name: "Test simpan",
+	}
 
-	c.JSON(http.StatusOK, users)
+	userRepository.Save(user)
 }
