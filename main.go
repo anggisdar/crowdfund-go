2d436675ed65117e44e468acd17bb15339a10038
anggis <anggisdar@gmail.com>
2024-11-17 13:51:55 +0700

    mengambil database ditabel user

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"webfunding/user"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -10,11 +11,23 @@ import (
 
 func main() {
 	dsn := "root:05072001@tcp(localhost:3306)/dbgo"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dbgo, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Connection to database conected")
+
+	var users []user.User
+
+	dbgo.Find(&users)
+
+	length := len(users)
+	fmt.Println(length)
+
+	for _, user := range users {
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+	}
 }
