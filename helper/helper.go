f8cdccf217695f8d98ec5e90cbca4619eb3210a2
anggis <anggisdar@gmail.com>
2024-11-18 16:36:12 +0700

    menambahkan file response struct Meta & Data

---
+++ helper/helper.go
@@ -0,0 +1,27 @@
+package helper
+
+type response struct {
+	Meta meta
+	Data interface{}
+}
+
+type meta struct {
+	Message string
+	Code    int
+	Status  string
+}
+
+func APIResponse(message string, code int, status string, data interface{}) response {
+	meta := meta{
+		Message: message,
+		Code:    code,
+		Status:  status,
+	}
+
+	JSONresponse := response{
+		Meta: meta,
+		Data: data,
+	}
+
+	return JSONresponse
+}
