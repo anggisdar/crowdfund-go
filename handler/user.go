110dbf3647e4280506c7670a55dcdda8bdbf0123
anggis <anggisdar@gmail.com>
2024-11-18 13:58:38 +0700

    menambahkan folder handler & file user.go

---
+++ handler/user.go
@@ -0,0 +1,21 @@
+package handler
+
+import (
+	"webfunding/user"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	// tangkap input dari user
+	// mapping input dari user ke struct RegisterUserInput
+	// struct di atas kita passing sebagai paramater service
+}
