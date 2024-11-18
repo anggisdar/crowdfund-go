05b43749e68f8124535fd03c996a20c3f984783a
anggis <anggisdar@gmail.com>
2024-11-18 16:07:11 +0700

    last update handler

--- handler/user.go
+++ handler/user.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"net/http"
 	"webfunding/user"
 
 	"github.com/gin-gonic/gin"
@@ -18,4 +19,18 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	// tangkap input dari user
 	// mapping input dari user ke struct RegisterUserInput
 	// struct di atas kita passing sebagai paramater service
+
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+	}
+
+	user, err := h.userService.RegisterUser(input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+	}
+
+	c.JSON(http.StatusOK, user)
 }
