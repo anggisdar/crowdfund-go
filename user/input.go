b1c500246a8968757cb0b237a161f5e28e4cc910
anggis <anggisdar@gmail.com>
2024-11-19 13:00:52 +0700

    git add tag & binding

--- user/input.go
+++ user/input.go
@@ -1,10 +1,10 @@
 package user
 
 type RegisterUserInput struct {
-	Name       string
-	Ocuppation string
-	Email      string
-	Password   string
+	Name       string `json:"name" binding:"required"`
+	Ocuppation string `json:"ocuppation" binding:"required"`
+	Email      string `json:"email" binding:"required,email"`
+	Password   string `json:"password" binding:"required"`
 }
 
 //ini struct user input untuk mapping
