2fa8fa4541cf5f2fc0ecfe3150981871b8e6b98a
anggis <anggisdar@gmail.com>
2024-11-24 19:17:44 +0700

    git add handler season login

--- handler/user.go
+++ handler/user.go
@@ -47,3 +47,40 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *userHandler) Login(c *gin.Context) {
+	//user memasukkan input (email & password) dari pengguna
+	//input ditangkap handler
+	//mapping dari input user ke struct
+	//dari input struct passing service
+	//di service mencari dengan bantuan repo user dengan email stb
+	//mencocokan password
+
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggedinUser, err := h.userService.Login(input)
+
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedinUser, "tokentokentoken")
+
+	response := helper.APIResponse("Successfuly login", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
