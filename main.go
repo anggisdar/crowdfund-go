96c6999bcbdcad0c9c1125f4bdfd78ac68db647e
anggis <anggisdar@gmail.com>
2024-11-24 19:08:26 +0700

    git add handler sessions login

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"webfunding/handler"
 	"webfunding/user"
@@ -22,26 +21,13 @@ func main() {
 	userRepository := user.NewRepository(dbgo)
 	userService := user.NewService(userRepository)
 
-	input := user.LoginInput{
-		Email:    "anggisdar@gmail.com",
-		Password: "password",
-	}
-
-	user, err := userService.Login(input)
-	if err != nil {
-		fmt.Println("Terjadi kesalahan")
-		fmt.Println(err.Error())
-	}
-
-	fmt.Println(user.Email)
-	fmt.Println(user.Name)
-
 	userHandler := handler.NewUserHandler(userService)
 
 	router := gin.Default()
 	api := router.Group("/api/v1")
 
 	api.POST("/users", userHandler.RegisterUser)
+	api.POST("/sessions", userHandler.Login)
 
 	router.Run()
 }
